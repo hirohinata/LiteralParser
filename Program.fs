56e8ee30519c07336fd23349caab525cb0229943
hirohinata <azuri_hirohinata@hotmail.co.jp>
2022-09-25 07:23:30 +0900

    first commit

---
+++ Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let Parse text =
+    text + "!"
+
+[<EntryPoint>]
+let main argv =
+    while true do
+        stdin.ReadLine()
+        |> Parse
+        |> stdout.WriteLine
+
+    0 // return an integer exit code
