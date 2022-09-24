ad9cb5b16138ea71f075528b7fc3dec3f8489af3
hirohinata <azuri_hirohinata@hotmail.co.jp>
2022-09-25 08:56:32 +0900

    add hex, octal, binary int parser

--- Program.fs
+++ Program.fs
@@ -5,6 +5,7 @@ open System
 type Token =
     | Identifier of str: string
     | Symbol of sym: Symbol
+    | HexNumber of num: string
     | Number of num: string
 and Symbol =
     | Sharp
@@ -13,24 +14,39 @@ and Symbol =
     | Minus
 
 let Lex (text: string) =
-    let rec find (text: string) index length cnd =
-        if length <= index then
-            index
-        elif cnd text.[index] then
-            find text (index + 1) length cnd
-        else
-            index
     let rec f (text: string) index length result =
         if length <= index then
             result
         else
             match text.[index] with
             | c when 'A' <= c && c <= 'Z' || 'a' <= c && c <= 'z' ->
-                let next = find text index length (fun c -> 'A' <= c && c <= 'Z' || 'a' <= c && c <= 'z')
-                f text next length (Identifier (text.Substring(index, next - index)) :: result)
+                let rec find (text: string) index length isHexNum =
+                    if length <= index then
+                        index, isHexNum
+                    else
+                        match text.[index] with
+                        | c when '0' <= c && c <= '9' || 'A' <= c && c <= 'F' || 'a' <= c && c <= 'f' || c = '_' ->
+                            find text (index + 1) length isHexNum
+                        | c when 'G' <= c && c <= 'Z' || 'g' <= c && c <= 'z' ->
+                            find text (index + 1) length false
+                        | _ -> index, isHexNum
+                match find text index length true with
+                | next, false -> f text next length (Identifier (text.Substring(index, next - index)) :: result)
+                | next, true -> f text next length (HexNumber (text.Substring(index, next - index)) :: result)
             | c when '0' <= c && c <= '9' ->
-                let next = find text index length (fun c -> '0' <= c && c <= '9')
-                f text next length (Number (text.Substring(index, next - index)) :: result)
+                let rec find (text: string) index length isHexNum =
+                    if length <= index then
+                        index, isHexNum
+                    else
+                        match text.[index] with
+                        | c when '0' <= c && c <= '9' || c = '_' ->
+                            find text (index + 1) length isHexNum
+                        | c when 'A' <= c && c <= 'F' || 'a' <= c && c <= 'f' ->
+                            find text (index + 1) length true
+                        | _ -> index, isHexNum
+                match find text index length false with
+                | next, false -> f text next length (Number (text.Substring(index, next - index)) :: result)
+                | next, true -> f text next length (HexNumber (text.Substring(index, next - index)) :: result)
             | '#' ->
                 f text (index + 1) length (Symbol Sharp :: result)
             | '.' ->
@@ -45,26 +61,32 @@ let Lex (text: string) =
     f text 0 text.Length []
     |> List.rev
     
-let (|Unsigned_Int|_|)= function
+let (|Unsigned_Int|_|) = function
     | Number num :: xs -> Some(num, xs)
     | _ -> None
 
-let (|Signed_Int|_|)= function
+let (|Signed_Int|_|) = function
    | Unsigned_Int(num, xs) -> Some(num, xs)
    | Symbol Plus :: Unsigned_Int(num, xs) -> Some(num, xs)
    | Symbol Minus:: Unsigned_Int(num, xs) -> Some("-" + num, xs)
    | _ -> None
 
+let (|Base_Number_Specified_Int|_|) = function
+    | Number baseNum :: Symbol Sharp :: Unsigned_Int(num, xs) -> Some(baseNum, num, xs)
+    | Number baseNum :: Symbol Sharp :: HexNumber num :: xs -> Some(baseNum, num, xs)
+    | _ -> None
+
 let(|Int_Literal|_|) = function
-    | Signed_Int(num, xs) -> Some(num, xs)
+    | Base_Number_Specified_Int(baseNum, num, xs) -> Some(baseNum, num, xs)
+    | Signed_Int(num, xs) -> Some("10", num, xs)
     | _ -> None
 
 let (|Numeric_Literal|_|) = function
-    | Int_Literal(num, xs) -> Some(num, xs)
+    | Int_Literal(baseNum, num, xs) -> Some(baseNum, num, xs)
     | _ -> None
 
 let Parse = function
-    | Numeric_Literal(num, []) -> $"Numeric {num}"
+    | Int_Literal(baseNum, num, []) -> $"Numeric {baseNum}#{num}"
     | tokens -> $"Parse Error : {tokens}"
 
 [<EntryPoint>]
